118b04eaff62105f51f177c205fe03e2910b1fba
ArteZee <98776298+ArteZee@users.noreply.github.com>
2022-07-13 22:38:06 +0300

    Add files via upload

--- hw_1/artem_task_1_hw_1.py
+++ hw_1/artem_task_1_hw_1.py
@@ -4,16 +4,30 @@ from typing import Any, Dict, List
 def filter_by_first_met_value(dataset: List[Dict[str, Any]], keys: List[str]):
     """Filter dataset by first met value in keys"""
 
-
+    # list of values of key in keys
+    list_values: list = []
     # list of dicts with unique values
     result: list = []
-    # add dict to result if dict have key in keys
+    # this list help us to delete duplicate values
+    used_values: list = []
+
     for element in dataset:
-        result = [element for key, value in element.items() if key in keys]
+        # append all values of keys
+        list_values.append([value for key, value in element.items() if key in keys])
+        for i in list_values:
+            # check our unique values in dataset dicts
+            # and append in "used_values" - values that we add that add dict in  result
+            if any(x in i for x in element.values()) and i not in used_values:
+                result.append(element)
+                used_values.append(i)
+
     return result
 
+
 print(filter_by_first_met_value([
-    {"foo": "FOO", "bar": "BAR", "foobar": "fb"},
-    {"foo": "F", "bar": "BAR", "foobar": "fb"},
-    {"foo": "FOO", "bar": "BAR", "foobar": "fb"},
-], ["foobar"]))
+    {"name": "Serhii", "company": "SoftServe", "job": "Software Engineer"},
+    {"name": "Serhii", "company": "Hillel", "job": "Python Trainer"},
+    {"name": "Vlad", "company": "SoftServe", "job": "Release Manager"},
+    {"name": "Vlad", "company": "A-Level", "job": "Python Trainer"},
+    {"name": "Serhii", "company": "A-Level", "job": "Python Trainer"},
+], ["name", "job"]))
